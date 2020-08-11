e3068376b123ce24a0e42a1f5bae9d94cbce1c3f
GhaloGhertje <koenvdh7@gmail.com>
2020-08-11 23:47:23 +0200

    Update

---
+++ 2D air hockey/Assets/Scripts/Score2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score2 : MonoBehaviour
+{
+    public int score;
+    public Text GreenScore;
+    public GameObject Ball;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        AddScore();
+    }
+    void AddScore()
+    {
+        score++;
+        GreenScore.text = score.ToString();
+    }
+
+    void BallRespawn()
+    {
+        GameObject a = Instantiate(Ball) as GameObject;
+    }
+}
---
+++ Unity Platformer Test/Assets/Scripts/Respawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Effect;
+    private bool isRespawning;
+    private Vector3 respawnPoint;
+
+    void Start()
+    {
+        respawnPoint = Player.transform.position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            {
+                GameObject e = Instantiate(Effect) as GameObject;
+                respawn(); //functie
+            }
+        }
+    }
+
+    public void respawn() //functie die uitgevoerd wordt
+    {
+        Player.transform.position = respawnPoint;
+    }
+}
